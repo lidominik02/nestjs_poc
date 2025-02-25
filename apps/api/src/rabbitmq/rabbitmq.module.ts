d4a52ea7848edb6ef359d9252c3d7916e0c42e92
Li Dominik <lidominik02@gmail.com>
2025-02-25 17:28:16 +0100

    feat: Add docker to web and fix api docker

--- apps/api/src/rabbitmq/rabbitmq.module.ts
+++ apps/api/src/rabbitmq/rabbitmq.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [RabbitMQService],
   exports: [RabbitMQService],
 })
